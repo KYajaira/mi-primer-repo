576d0d0ad7901789cdadba0ad79158cf6c576982
Karen <kyajaira2003@gmail.com>
2026-05-20 18:32:38 -0500

    Agregando comida y color favorito

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 let nombre = "Karen";
+let edad = 22;
+let ciudad = "Cancún";
 
-console.log("Hola " + nombre);
-console.log("Estoy aprendiendo Git y GitHub");
+console.log("Hola, mi nombre es " + nombre);
+console.log("Tengo " + edad + " años");
+console.log("Vivo en " + ciudad);
+let comida = "pizza";
+let color = "morado";
+
+console.log("Mi comida favorita es " + comida);
+console.log("Mi color favorito es " + color);
