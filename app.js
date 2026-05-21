b4600efc542fc79890116b7dfa02fdb7a967bb62
Karen <kyajaira2003@gmail.com>
2026-05-21 17:17:11 -0500

    Agregando ejercicios condicionales

--- app.js
+++ app.js
@@ -10,3 +10,30 @@ let color = "morado";
 
 console.log("Mi comida favorita es " + comida);
 console.log("Mi color favorito es " + color);
+
+
+if (edad >= 18) {
+    console.log("Puedo entrar a la fiesta");
+} else {
+    console.log("No puedo entrar");
+}
+let temperatura = 35;
+
+if (temperatura > 30) {
+    console.log("Hace mucho calor");
+} else {
+    console.log("El clima está agradable");
+}
+let dinero = 150;
+if (dinero >= 100) {
+    console.log("Puedo comprarme unos audifonos");
+} else {
+    console.log ("No me alcanza");
+
+}
+let contraseña= "1234";
+if (contraseña == "1234") {
+    console.log("Acceso permitido");
+} else {
+    console.log("Denegado");
+}
